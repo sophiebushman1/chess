f3c23d023a1f4bfc50e30fa05ad62fe1f6448ae3
SophiaTaylor7 <sophia7taylor@gmail.com>
2025-09-19 10:19:03 -0600

    Setting up Chess Position to check the input and set the row and column

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,6 @@
 package chess;
 
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -7,8 +8,20 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int row;
+    private final int column;
+    //Rows and columns are 1-8 where column 1 is left and row 1 is bottom. This sets up our row and column variable that will eventually be returned
 
     public ChessPosition(int row, int col) {
+        //Make sure the row and col passed in fit the board format
+        if (row<1 || col<1) {
+            throw new IllegalArgumentException("rows and columns must be 1 or less than 1");
+        }
+        if (row>8 || col>8) {
+            throw new IllegalArgumentException("rows and columns must be 8 or less than 8");
+        }
+        this.row = row;
+        this.column = col;
     }
 
     /**
@@ -16,7 +29,8 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+
+        return row;
     }
 
     /**
@@ -24,6 +38,7 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+
+        return column;
     }
 }
